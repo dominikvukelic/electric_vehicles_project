5fb0b318f9ef4ca6ec494d3dabc010deeb05d9fe
Dominik Vukelic <letotalhavoc@gmail.com>
2024-04-29 16:01:30 +0200

    started a new repo

---
+++ cleaning_starting_file/ev_cleaning.R
@@ -0,0 +1,11 @@
+# Importing necessary libraries
+library(tidyverse)
+
+# Specify the relative path to your dataset within the project
+dataset_path <- "starting_file/EV_data.csv"
+
+# Import the dataset using read.csv with a.strings parameter to indicate the values that should be treated as missing (blank)
+df <- read.csv(dataset_path, na.strings = c("", "NA"))
+
+# View the contents of a dataframe
+View(df)
