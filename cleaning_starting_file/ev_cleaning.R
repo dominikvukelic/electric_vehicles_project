224084ef235b2449b0fafed56c62816dacff490a
Dominik Vukelic <letotalhavoc@gmail.com>
2024-04-30 13:46:12 +0200

    cleaning dataframe

--- cleaning_starting_file/ev_cleaning.R
+++ cleaning_starting_file/ev_cleaning.R
@@ -18,13 +18,21 @@ glimpse(df)
 df <- df %>%
   rename(Acceleration = Accel)
 
-# Renaming Acceleration column to Acceleration_to_100km/h_in_seconds
+# Renaming Acceleration column to Acceleration_to_100km_h_in_seconds
 df <- df %>%
   rename(Acceleration_to_100km_h_in_seconds = Acceleration)
 
-# Removing sec value from Acceleration_to_100km/h_in_seconds column
+# Removing sec value from Acceleration_to_100km_h_in_seconds column
 df <- df %>%
   mutate(Acceleration_to_100km_h_in_seconds = gsub(" sec", "", Acceleration_to_100km_h_in_seconds))
 
 # Convert the column to numeric
 df$Acceleration_to_100km_h_in_seconds <- as.numeric(df$Acceleration_to_100km_h_in_seconds)
+
+# Renaming TopSpeed column to Top_speed_in_km/h
+df <- df %>%
+  rename(Top_speed_in_km_h = TopSpeed)
+
+# Removing km/h value from Top_speed_in_km_h column
+df <- df %>%
+  mutate(Top_speed_in_km_h = gsub(" km/h", "", Top_speed_in_km_h))
